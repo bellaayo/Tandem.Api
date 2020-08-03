9a9500bde764c8019705e3ccbaa444bdcd14d0b8
Yosef Biadglegn <yosef.t.biadglegn@gmail.com>
2020-08-03 11:19:24 -0700

    Added configuration, added folder structure, inital startup setup

--- Startup.cs
+++ Startup.cs
@@ -10,11 +10,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Tandem.API.Data.Repositories;
+using Tandem.API.Services;
 
 namespace Tandem.API
 {
     public class Startup
     {
+        private AppSettings AppSettings => Configuration.Get<AppSettings>();
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +29,10 @@ namespace Tandem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .Configure<AppSettings>(Configuration)
+                .AddTransient<IUsersService, UsersService>()
+                .AddTransient<IUserRepository, UserRepository>();
             services.AddControllers();
         }
 
